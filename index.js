9f6d052f33c5107cc5dde16ed5f9efc7777f520d
bytil70 <bytil70@gmail.com>
2025-03-28 02:26:04 +0000

    update

--- index.js
+++ index.js
@@ -1,10 +1,61 @@
 //document.getElementById('count-el').innerText = 5
 
-let firstBatch = 5
-let secondBatch = 7
+//let firstBatch = 5
+//let secondBatch = 7
 
-let count = firstBatch * secondBatch
-console.log(count)
+//let count = firstBatch * secondBatch
+//console.log(count)
 
-let myage = 70
-console.log(myage)
+//let myage = 70
+//console.log(myage)
+
+//let myAge = 35
+//let humanDogRatio = 7
+
+//let myDogAge = myAge * humanDogRatio
+//console.log(myDogAge)
+
+function increment() {
+    console.log('The button was clicked')
+}
+
+function countdown() {
+    console.log(5)
+    console.log(4)
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+//countdown()
+
+function myLogger() {
+
+}
+
+let lap1 = 34
+let lap2 = 33
+let lap3 = 36
+
+function logLapTime() {
+    let totalTime = lap1 + lap2 + lap3
+    console.log(totalTime)
+}
+
+//logLapTime()
+
+let countEl = document.getElementById('count-el') . //this is called model
+
+console.log(countEl)
+
+let count = 0
+function increment() {
+    count = count + 1
+    countEl.innerText = count
+    
+}
+
+function save() {
+    console.log(count)
+}
+
